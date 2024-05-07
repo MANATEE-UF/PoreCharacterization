dcb82d01130da3aab93be9cd98629b09c40b1aec
mitchellmika <mitchellmika07@gmail.com>
2024-05-06 22:27:26 -0400

    added reading for 4061 umo sample combine all histograms to one plot remove sample images add 4061 JSON file

--- UMo_GBS/UMoGBS.py
+++ UMo_GBS/UMoGBS.py
@@ -4,19 +4,22 @@ import matplotlib.pyplot as plt
 import matplotlib.patheffects as pe
 import json
 from tqdm import tqdm
+import scipy
 
 def main():
 
     scale = 20/161 # For 760kx, nm/pixel
     areaScale = scale**2 # nm^2 / pixel^2
 
-    jsonFile = open("UMo_GBS/gbs.json")
+    # Read 4039 data (low burnup)
+    jsonFile = open("UMo_GBS/KGT4039.json")
 
     data = json.load(jsonFile)
     imagesData = data["images"]
     annotationData = data["annotations"]
 
-    areas = {"A":[], "C":[], "D":[], "E":[]}
+    areas = {"4039" : [], "4039 A":[], "4039 C":[], "4039 D":[], "4039 E":[], 
+             "4061" : [], "4061 A":[], "4061 B":[], "4061 C":[], "4061 D":[]}
     totalAreas = []
     for i, bubble in enumerate(tqdm(annotationData, desc="Calculating Bubble Areas")):
         bubbleVertices = np.array(bubble["segmentation"][0])
@@ -31,30 +34,105 @@ def main():
 
         gridID = imagesData[bubble["image_id"]]["file_name"].split("_")[1]
 
-        areas[gridID].append(sum * areaScale)
-        totalAreas.append(sum * areaScale)
+        area = sum * areaScale
 
-    # Area Box Plot
-    stacked = False
-    if stacked:
-        totalAreas = []
-        totalAreas.append(areas["A"])
-        totalAreas.append(areas["C"])
-        totalAreas.append(areas["D"])
-        totalAreas.append(areas["E"])
-        plt.hist(totalAreas, bins=100, histtype="bar", stacked=True, label=["Post A", "Post C", "Post D", "Post E"])
-    else:
-        plt.hist(areas["A"], label="Post A", bins=100, alpha=0.5)
-        plt.hist(areas["C"], label="Post C", bins=100, alpha=0.5)
-        plt.hist(areas["D"], label="Post D", bins=100, alpha=0.5)
-        plt.hist(areas["E"], label="Post E", bins=100, alpha=0.5)
-    
-    plt.legend()
-    plt.xlabel("Area (nm^2)")
-    plt.ylabel("Frequency")
+        # Trim outliers, bad segmentation
+        if area > 35:
+            continue
+
+        areas["4039"].append(area)
+        areas[f"4039 {gridID}"].append(area)
+        totalAreas.append(area)
+
+    # Read 4061 data (high burnup)
+    jsonFile = open("UMo_GBS/KGT4061.json")
+
+    data = json.load(jsonFile)
+    imagesData = data["images"]
+    annotationData = data["annotations"]
+
+    for i, bubble in enumerate(tqdm(annotationData, desc="Calculating Bubble Areas")):
+        bubbleVertices = np.array(bubble["segmentation"][0])
+        bubbleVerticesX = bubbleVertices[0::2]
+        bubbleVerticesY = bubbleVertices[1::2]
+        sum = 0
+        for j in range(len(bubbleVerticesX)-1):
+            sum += ((bubbleVerticesX[j] * bubbleVerticesY[j+1]) - (bubbleVerticesX[j+1] * bubbleVerticesY[j]))
+        sum += ((bubbleVerticesX[-1] * bubbleVerticesY[0]) - (bubbleVerticesX[0] * bubbleVerticesY[-1]))
+        sum = np.abs(sum) 
+        sum /= 2
+
+        gridID = imagesData[bubble["image_id"]]["file_name"].split("_")[0]
+
+        area = sum * areaScale
+
+        # Trim outliers, bad segmentation
+        if area > 35:
+            continue
+
+        areas["4061"].append(area)
+        areas[f"4061 {gridID}"].append(area)
+        totalAreas.append(area)
+
+    # indexing along 
+    areaByPost = []
+    areaByPostLabel = ["4039 A", "4039 C", "4039 D", "4039 E", "4061 A", "4061 B", "4061 C", "4061 D"]
+    for label in areaByPostLabel:
+        areaByPost.append(areas[label])
+
+    # indexing along first axis gives the area distribution for a given burnup
+    areaByBurnup = []
+    areaByBurnupLabel = ["4039", "4061"]
+    areaByBurnup.append(areas["4039"])
+    areaByBurnup.append(areas["4061"])
+
+    fig, axs = plt.subplots(2,2)
+
+    axs[0][0].hist(areaByPost, bins=100, histtype="bar", stacked=True, label=areaByPostLabel)
+    axs[1][0].hist(areaByBurnup, bins=100, histtype="bar", stacked=True, label=areaByBurnupLabel)
+    axs[0][1].hist(areas["4039 A"], label="4039 A", bins=100, alpha=0.5)
+    axs[0][1].hist(areas["4039 C"], label="4039 C", bins=100, alpha=0.5)
+    axs[0][1].hist(areas["4039 D"], label="4039 D", bins=100, alpha=0.5)
+    axs[0][1].hist(areas["4039 E"], label="4039 E", bins=100, alpha=0.5)
+    axs[0][1].hist(areas["4061 A"], label="4061 A", bins=100, alpha=0.5)
+    axs[0][1].hist(areas["4061 B"], label="4061 B", bins=100, alpha=0.5)
+    axs[0][1].hist(areas["4061 C"], label="4061 C", bins=100, alpha=0.5)
+    axs[0][1].hist(areas["4061 D"], label="4061 D", bins=100, alpha=0.5)
+
+    axs[1][1].hist(areaByBurnup[0], label="4039", bins=100, alpha=0.5)
+    axs[1][1].hist(areaByBurnup[1], label="4061", bins=100, alpha=0.5)
+
+    axs[0][0].legend()
+    axs[0][1].legend()
+    axs[1][0].legend()
+    axs[1][1].legend()
+    axs[1][0].set_xlabel("Area (nm^2)")
+    axs[1][1].set_xlabel("Area (nm^2)")
+    axs[0][0].set_ylabel("Frequency")
+    axs[1][0].set_ylabel("Frequency")
     plt.title("GBS Area Distribution")
 
     plt.show()
+
+    distribution1 = "4039 A"
+    distribution2 = "4039 D"
+    altHyp = "lesser"
+    print()
+    print(f"K-S test between {distribution1} and {distribution2}")
+    ksTest = scipy.stats.ks_2samp(areas[distribution1], areas[distribution2], alternative=altHyp)
+    if altHyp == "two-sided":
+        if ksTest.pvalue < 0.05:
+            print(f"{distribution1} and {distribution2} do not have identical distribution")
+        else:
+            print(f"{distribution1} and {distribution2} have identical distribution")
+    else:
+        if ksTest.pvalue < 0.05:
+            print(f"{distribution2} is {altHyp} than {distribution1}")
+        else:
+            print(f"{distribution2} is not {altHyp} than {distribution1}")
+    print(f"Test statistic: {ksTest.statistic}")
+    print(f"p-value: {ksTest.pvalue}")
+    print()
     
 
 if __name__ == "__main__":
