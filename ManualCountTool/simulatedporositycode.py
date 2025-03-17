2db7680fa6a0e6bf4686ad24de40759d87ca4baf
mitchellmika <mitchellmika07@gmail.com>
2025-03-17 15:25:35 -0400

    update to sim image generation code

--- ManualCountTool/simulatedporositycode.py
+++ ManualCountTool/simulatedporositycode.py
@@ -26,9 +26,9 @@ def draw_image(image_size_x, image_size_y):
     img = Image.new('L', (image_size_x, image_size_y), color=255) #creates image for plotting
     return img
 
-def small(targetPorosity, image_size_x, image_size_y):
-    min_radius = 2 #units: pixels (chosen to allow to be seen using manual counting program)
-    max_radius = 34/2 #units: pixels (chosen based on small pores being smaller than 6.4 microns in diameter in literature)
+def small(targetPorosity, image_size_x, image_size_y, boundingbox_x=None, boundingbox_y=None):
+    min_radius = 2 #units: pixels
+    max_radius = 25 #units: pixels
     total_area = []
     drawn_positions = []
     img = draw_image(image_size_x, image_size_y)
@@ -37,19 +37,22 @@ def small(targetPorosity, image_size_x, image_size_y):
         radius_x, radius_y = random_radius(min_radius, max_radius)
         area = ellipse_area(radius_x, radius_y)
         total_area.append(area)
-        grayscale_value = random.randint(0, 150) #gives random grayscale value to fill each ellipse (no white values allowed)
-        center_x, center_y = random_position(image_size_x, image_size_y)
+        grayscale_value = 50 #gives random grayscale value to fill each ellipse (no white values allowed)
+        if boundingbox_x is not None:
+            center_x, center_y = random_position(boundingbox_x, boundingbox_y)
+        else:
+            center_x, center_y = random_position(image_size_x, image_size_y)
         drawn_positions.append((center_x, center_y, max(radius_x, radius_y))) #save where each ellipse is drawn
         draw.ellipse([(center_x - radius_x, center_y - radius_y),
                     (center_x + radius_x, center_y + radius_y)],
-                    fill=grayscale_value, outline=0)
+                    fill=grayscale_value)
     img_array = np.array(img)
     real_porosity = porositycalc(img_array, image_size_x, image_size_y)
     return Image.fromarray(img_array), real_porosity
 
-def medium(targetPorosity, image_size_x, image_size_y):
-    min_radius = 36/2 #units: pixels (chosen to allow to be seen using manual counting program)
-    max_radius = 50 #units: pixels (chosen based on medium pores being between 6.4 and 16.2 microns in diameter in literature)
+def medium(targetPorosity, image_size_x, image_size_y, boundingbox_x=None, boundingbox_y=None):
+    min_radius = 26 #units: pixels
+    max_radius = 50 #units: pixels
     total_area = []
     drawn_positions = []
     img = draw_image(image_size_x, image_size_y)
@@ -58,19 +61,22 @@ def medium(targetPorosity, image_size_x, image_size_y):
         radius_x, radius_y = random_radius(min_radius, max_radius)
         area = ellipse_area(radius_x, radius_y)
         total_area.append(area)
-        grayscale_value = random.randint(0,150) #gives random grayscale value to fill each ellipse (no white values allowed)
-        center_x, center_y = random_position(image_size_x, image_size_y)
+        grayscale_value = 50 #gives random grayscale value to fill each ellipse (no white values allowed)
+        if boundingbox_x is not None:
+            center_x, center_y = random_position(boundingbox_x, boundingbox_y)
+        else:
+            center_x, center_y = random_position(image_size_x, image_size_y)
         drawn_positions.append((center_x, center_y, max(radius_x, radius_y))) #save where each ellipse is drawn
         draw.ellipse([(center_x - radius_x, center_y - radius_y),
                     (center_x + radius_x, center_y + radius_y)],
-                    fill=grayscale_value, outline=0)
+                    fill=grayscale_value)
     img_array = np.array(img)
     real_porosity = porositycalc(img_array, image_size_x, image_size_y)
     return Image.fromarray(img_array), real_porosity
 
-def large(targetPorosity, image_size_x, image_size_y):
-    min_radius = 26 #units: pixels (chosen to allow to be seen using manual counting program)
-    max_radius = 75 #units: pixels (arbitrarily chosen so that pores aren't ridiculously large)
+def large(targetPorosity, image_size_x, image_size_y, boundingbox_x=None, boundingbox_y=None):
+    min_radius = 51 #units: pixels
+    max_radius = 100 #units: pixels
     total_area = []
     drawn_positions = []
     img = draw_image(image_size_x, image_size_y)
@@ -79,17 +85,20 @@ def large(targetPorosity, image_size_x, image_size_y):
         radius_x, radius_y = random_radius(min_radius, max_radius)
         area = ellipse_area(radius_x, radius_y)
         total_area.append(area)
-        grayscale_value = random.randint(0,150) #gives random grayscale value to fill each ellipse (no white values allowed)
-        center_x, center_y = random_position(image_size_x, image_size_y)
+        grayscale_value = 50 #gives random grayscale value to fill each ellipse (no white values allowed)
+        if boundingbox_x is not None:
+            center_x, center_y = random_position(boundingbox_x, boundingbox_y)
+        else:
+            center_x, center_y = random_position(image_size_x, image_size_y)
         drawn_positions.append((center_x, center_y, max(radius_x, radius_y))) #save where each ellipse is drawn
         draw.ellipse([(center_x - radius_x, center_y - radius_y),
                     (center_x + radius_x, center_y + radius_y)],
-                    fill=grayscale_value, outline=0)
+                    fill=grayscale_value)
     img_array = np.array(img)
     real_porosity = porositycalc(img_array, image_size_x, image_size_y)
     return Image.fromarray(img_array), real_porosity
 
-def mixed(targetPorosity, image_size_x, image_size_y):
+def mixed(targetPorosity, image_size_x, image_size_y, boundingbox_x=None, boundingbox_y=None):
     total_area = []
     drawn_positions = []
     img = draw_image(image_size_x, image_size_y)
@@ -98,114 +107,25 @@ def mixed(targetPorosity, image_size_x, image_size_y):
         size = random.randint(1, 3)
         if size == 1: #small pore
             min_radius = 2
-            max_radius = 34/2
+            max_radius = 25
         elif size == 2: #medium pore
-            min_radius = 36/2
+            min_radius = 26
             max_radius = 50
         elif size == 3: #large pore
-            min_radius = 52/2
+            min_radius = 51
             max_radius = 100
         radius_x, radius_y = random_radius(min_radius, max_radius)
         area = ellipse_area(radius_x, radius_y)
         total_area.append(area)
-        grayscale_value = random.randint(0,150) #gives random grayscale value to fill each ellipse (no white values allowed)
-        center_x, center_y = random_position(image_size_x, image_size_y)
+        grayscale_value = 50 #gives random grayscale value to fill each ellipse (no white values allowed)
+        if boundingbox_x is not None:
+            center_x, center_y = random_position(boundingbox_x, boundingbox_y)
+        else:
+            center_x, center_y = random_position(image_size_x, image_size_y)
         drawn_positions.append((center_x, center_y, max(radius_x, radius_y))) #save where each ellipse is drawn
         draw.ellipse([(center_x - radius_x, center_y - radius_y),
                     (center_x + radius_x, center_y + radius_y)],
-                    fill=grayscale_value, outline=0)
-    img_array = np.array(img)
-    real_porosity = porositycalc(img_array, image_size_x, image_size_y)
-    return Image.fromarray(img_array), real_porosity
-
-def smallclustered(targetPorosity, image_size_x, image_size_y, boundingbox_x, boundingbox_y):
-    min_radius = 2 #units: pixels (chosen to allow to be seen using manual counting program)
-    max_radius = 34/2 #units: pixels (chosen based on small pores being smaller than 6.4 microns in diameter in literature)
-    total_area = []
-    drawn_positions = []
-    img = draw_image(image_size_x, image_size_y)
-    draw = ImageDraw.Draw(img)
-    while porositycalc(np.array(img), image_size_x, image_size_y) <= targetPorosity:
-        radius_x, radius_y = random_radius(min_radius, max_radius)
-        area = ellipse_area(radius_x, radius_y)
-        total_area.append(area)
-        grayscale_value = random.randint(0,150) #gives random grayscale value to fill each ellipse (no white values allowed)
-        center_x, center_y = random_position(boundingbox_x, boundingbox_y)
-        drawn_positions.append((center_x, center_y, max(radius_x, radius_y))) #save where each ellipse is drawn
-        draw.ellipse([(center_x - radius_x, center_y - radius_y),
-                    (center_x + radius_x, center_y + radius_y)],
-                    fill=grayscale_value, outline=0)
-    img_array = np.array(img)
-    real_porosity = porositycalc(img_array, image_size_x, image_size_y)
-    return Image.fromarray(img_array), real_porosity
-
-def mediumclustered(targetPorosity, image_size_x, image_size_y, boundingbox_x, boundingbox_y):
-    min_radius = 36/2 #units: pixels (chosen to allow to be seen using manual counting program)
-    max_radius = 50 #units: pixels (chosen based on medium pores being between 6.4 and 16.2 microns in diameter in literature)
-    total_area = []
-    drawn_positions = []
-    img = draw_image(image_size_x, image_size_y)
-    draw = ImageDraw.Draw(img)
-    while porositycalc(np.array(img), image_size_x, image_size_y) <= targetPorosity:
-        radius_x, radius_y = random_radius(min_radius, max_radius)
-        area = ellipse_area(radius_x, radius_y)
-        total_area.append(area)
-        grayscale_value = random.randint(0,150) #gives random grayscale value to fill each ellipse (no white values allowed)
-        center_x, center_y = random_position(boundingbox_x, boundingbox_y)
-        drawn_positions.append((center_x, center_y, max(radius_x, radius_y))) #save where each ellipse is drawn
-        draw.ellipse([(center_x - radius_x, center_y - radius_y),
-                    (center_x + radius_x, center_y + radius_y)],
-                    fill=grayscale_value, outline=0)
-    img_array = np.array(img)
-    real_porosity = porositycalc(img_array, image_size_x, image_size_y)
-    return Image.fromarray(img_array), real_porosity
-
-def largeclustered(targetPorosity, image_size_x, image_size_y, boundingbox_x, boundingbox_y):
-    min_radius = 52/2 #units: pixels (chosen to allow to be seen using manual counting program)
-    max_radius = 75 #units: pixels (arbitrarily chosen so that pores aren't ridiculously large)
-    total_area = []
-    drawn_positions = []
-    img = draw_image(image_size_x, image_size_y)
-    draw = ImageDraw.Draw(img)
-    while porositycalc(np.array(img), image_size_x, image_size_y) <= targetPorosity:
-        radius_x, radius_y = random_radius(min_radius, max_radius)
-        area = ellipse_area(radius_x, radius_y)
-        total_area.append(area)
-        grayscale_value = random.randint(0,150) #gives random grayscale value to fill each ellipse (no white values allowed)
-        center_x, center_y = random_position(boundingbox_x, boundingbox_y)
-        drawn_positions.append((center_x, center_y, max(radius_x, radius_y))) #save where each ellipse is drawn
-        draw.ellipse([(center_x - radius_x, center_y - radius_y),
-                    (center_x + radius_x, center_y + radius_y)],
-                    fill=grayscale_value, outline=0)
-    img_array = np.array(img)
-    real_porosity = porositycalc(img_array, image_size_x, image_size_y)
-    return Image.fromarray(img_array), real_porosity
-
-def mixedclustered(targetPorosity, image_size_x, image_size_y, boundingbox_x, boundingbox_y):
-    total_area = []
-    drawn_positions = []
-    img = draw_image(image_size_x, image_size_y)
-    draw = ImageDraw.Draw(img)
-    while porositycalc(np.array(img), image_size_x, image_size_y) <= targetPorosity:
-        size = random.randint(1, 3)
-        if size == 1: #small pore
-            min_radius = 2
-            max_radius = 34/2
-        elif size == 2: #medium pore
-            min_radius = 36/2
-            max_radius = 50
-        elif size == 3: #large pore
-            min_radius = 52/2
-            max_radius = 100
-        radius_x, radius_y = random_radius(min_radius, max_radius)
-        area = ellipse_area(radius_x, radius_y)
-        total_area.append(area)
-        grayscale_value = random.randint(0,150) #gives random grayscale value to fill each ellipse (no white values allowed)
-        center_x, center_y = random_position(boundingbox_x, boundingbox_y)
-        drawn_positions.append((center_x, center_y, max(radius_x, radius_y))) #save where each ellipse is drawn
-        draw.ellipse([(center_x - radius_x, center_y - radius_y),
-                    (center_x + radius_x, center_y + radius_y)],
-                    fill=grayscale_value, outline=0)
+                    fill=grayscale_value)
     img_array = np.array(img)
     real_porosity = porositycalc(img_array, image_size_x, image_size_y)
     return Image.fromarray(img_array), real_porosity
@@ -228,35 +148,35 @@ def main(set_porosity, size_dist, orientation):
         os.mkdir(dirName)
 
     bbox_min = np.sqrt(set_porosity/100) # ensures that bbox is large enough to fit enough pores to meet porosity. assumes square
-    randInts = []
+
     with open("key.csv", "a", newline="") as csvfile:
+        
         writer = csv.writer(csvfile)
         writer.writerow(["ID", "Target Porosity", "Real Porosity", "Pore sizes", "Random or Clustered"])
+        
         for i in range(num_images):
+
+            randInts = os.listdir(dirName)
             randInt = random.randint(100000,999999)
             while randInt in randInts:
                 randInt = random.randint(100000,999999)
+
             if orientation == 'random':
-                if size_dist == 'small':
-                    random_image, real_porosity = small(set_porosity, image_size_x, image_size_y)
-                elif size_dist == 'medium':
-                    random_image, real_porosity = medium(set_porosity, image_size_x, image_size_y)
-                elif size_dist == 'large':
-                    random_image, real_porosity = large(set_porosity, image_size_x, image_size_y)
-                elif size_dist == 'mixed':
-                    random_image, real_porosity = mixed(set_porosity, image_size_x, image_size_y)
+                boundingbox_x = None
+                boundingbox_y = None
             elif orientation == 'clustered':
                 #this creates a random size bounding box for pore placement on each image
                 boundingbox_x = random.randint(int(image_size_x*bbox_min), image_size_x)
                 boundingbox_y = random.randint(int(image_size_y*bbox_min), image_size_y)
-                if size_dist == 'small':
-                    random_image, real_porosity = smallclustered(set_porosity, image_size_x, image_size_y, boundingbox_x, boundingbox_y)
-                elif size_dist == 'medium':
-                    random_image, real_porosity = mediumclustered(set_porosity, image_size_x, image_size_y, boundingbox_x, boundingbox_y)
-                elif size_dist == 'large':
-                    random_image, real_porosity = largeclustered(set_porosity, image_size_x, image_size_y, boundingbox_x, boundingbox_y)
-                elif size_dist == 'mixed':
-                    random_image, real_porosity = mixedclustered(set_porosity, image_size_x, image_size_y, boundingbox_x, boundingbox_y)
+            
+            if size_dist == 'small':
+                random_image, real_porosity = small(set_porosity, image_size_x, image_size_y, boundingbox_x, boundingbox_y)
+            elif size_dist == 'medium':
+                random_image, real_porosity = medium(set_porosity, image_size_x, image_size_y, boundingbox_x, boundingbox_y)
+            elif size_dist == 'large':
+                random_image, real_porosity = large(set_porosity, image_size_x, image_size_y, boundingbox_x, boundingbox_y)
+            elif size_dist == 'mixed':
+                random_image, real_porosity = mixed(set_porosity, image_size_x, image_size_y, boundingbox_x, boundingbox_y)
             
             random_image.save(f"{dirName}/{randInt}.png")
             key_entry = [f"{randInt}",f"{set_porosity}" ,f"{real_porosity:.3f}", size_dist, orientation]
