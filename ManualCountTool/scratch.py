b3ae03e4a51c0536ce3c2a9c6edb10228cb8c83c
mitchellmika <mitchellmika07@gmail.com>
2025-03-17 13:45:25 -0400

    adding opt ratio hist plot

--- ManualCountTool/scratch.py
+++ ManualCountTool/scratch.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy
+from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
 
-# FIXME: Instead of reducing the MOE, make it one sided
-# If p=0.99 and MOE=0.05, set the upper CI to 1.0 and only care about the lower CI
+
 def PlotSamplesPerInitialGuess():
     porosityGuesses = np.linspace(0,1,21)
     porosityGuesses[0] = 0.01
@@ -15,7 +15,7 @@ def PlotSamplesPerInitialGuess():
         initialGuesses = np.ones(16) * porosityGuess
         highVarGuess = np.ones(16) * 0.5
         numStrata_N = 4
-        N=512*512
+        N=256*256
         MOE = 0.05
         e_moe = 0.01
         alpha = 0.95
@@ -63,6 +63,190 @@ def PlotSamplesPerInitialGuess():
     plt.scatter(porosityGuesses, samplesNeeded)
     plt.show()
 
+def PlotSampleFractionPerInitialConditions():
+    MOEs = [0.05, 0.03]
+    porosityGuesses = np.linspace(0,1,21)
+    porosityGuesses[0] = 0.01
+    porosityGuesses[-1] = 0.99
+    cs = ["r","g","b"]
+    for j in range(2):
+        samplesNeeded = []
+        maxIters = 10
+        for i in range(11):
+            porosityGuess = porosityGuesses[i]
+            initialGuesses = np.ones(16) * porosityGuess
+            highVarGuess = np.ones(16) * 0.5
+            numStrata_N = 4
+            N=256*256
+            MOE = MOEs[j]
+            e_moe = 0.1
+            alpha = 0.95
+            print(f"{i+1}/21: {100*porosityGuess:.2f}")
+
+            W_h = 1 / numStrata_N**2 # Value constant because image is split evenly. small differences neglected
+            initialStrataProportion = porosityGuess
+            variance = np.sum(W_h * np.sqrt(highVarGuess * (1 - highVarGuess)))**2 / numStrata_N**2 - ((1/N) * np.sum(W_h * highVarGuess * (1 - highVarGuess))) # highest variance given the initial guesses. ensures that first guess is very small
+            if initialStrataProportion > 0.5 and MOE > 1-initialStrataProportion:
+                MOE = ((1-initialStrataProportion)+MOE) / 2 # want to keep +- MOE as close as possible on the open side. so if p=0.01, with 5% MOE, the CI should be (0,0.06)
+                # print(f"MOE stretches beyond range of [0,1] based on initial guess, reducing to {MOE:.2f}")
+            elif initialStrataProportion < 0.5 and MOE > initialStrataProportion:
+                MOE = (initialStrataProportion + MOE) / 2
+                # print(f"MOE stretches beyond range of [0,1] based on initial guess, reducing to {initialStrataProportion:.2f}")
+            upperCL = 1.0
+            lowerCL = 0.0
+            withinTolerance = False
+            d = 0.5
+            currentIter = 0
+            while not withinTolerance and currentIter < maxIters:
+                n = np.sum(W_h * np.sqrt(initialGuesses * (1-initialGuesses)) / variance) # smaller variance, higher n
+                n = int(np.ceil(n))
+                lowerCL, upperCL = TwoSidedCL_A(n, initialStrataProportion, alpha)
+                lowerCL /= n
+                upperCL /= n
+
+                if ((upperCL - lowerCL) / 2) > MOE: # Eq.15 not satisfied
+                    variance *= d
+                else: # Eq. 15 satisfied
+                    pctDiff = abs((((upperCL - lowerCL) / 2) - MOE) / MOE)
+                    if pctDiff > e_moe: # variance too low, overestimating how many sample points needed
+                        variance /= d
+                        d += (1-d)*0.2
+                        variance *= d
+                    else:
+                        withinTolerance = True
+                currentIter += 1
+            samplesNeeded.append(n/N)
+            print(f"({lowerCL*100:.2f}, {upperCL*100:.2f})")
+            print(f"MOE:{100*((upperCL - lowerCL) / 2):.2f}")
+            print()
+        plt.plot(porosityGuesses[:11], samplesNeeded, c=cs[j], label=f"{MOE*100:.1f}% MOE")
+        plt.scatter(porosityGuesses[:11], samplesNeeded, c=cs[j])
+    plt.legend()
+    plt.show()
+
+def Plot_n_ratio():
+    plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "Times"
+    })
+
+    ratios = []
+    W_h = 1/16
+
+    initialGuesses = np.ones(16)
+    initialGuesses[0:4] = 0.05
+    initialGuesses[4:8] = 0.95
+    initialGuesses[8:] = 0.5
+    print(initialGuesses)
+    ratio = W_h * (np.sum(np.sqrt(initialGuesses * (1-initialGuesses))))**2 / np.sum(initialGuesses * (1-initialGuesses))
+    print(ratio)
+
+    for i in range(100000):
+        initialGuesses = np.random.choice([0.05, 0.125, 0.25, 0.375, 0.50, 0.675, 0.75, 0.875, 0.95], 16)
+
+        ratio = W_h * (np.sum(np.sqrt(initialGuesses * (1-initialGuesses))))**2 / np.sum(initialGuesses * (1-initialGuesses))
+
+        ratios.append(ratio)
+    
+    fig, axs = plt.subplots(1,2)
+    
+
+    bins = np.arange(0.85, 1.0, 0.01)
+    axs[0].hist(ratios, bins=bins, edgecolor="black")
+    axs[0].set_xlabel(r"$n_{opt} / n_{prop}$", fontsize=14)
+    axs[0].set_ylabel("Frequency", fontsize=14)
+    axs[0].xaxis.set_minor_locator(AutoMinorLocator())
+    axs[0].yaxis.set_minor_locator(AutoMinorLocator())
+    axs[0].set_xlim(0.85,1.0)
+    axs[0].tick_params(axis="both", which="major", labelsize=12)
+
+    axs[1].ecdf(ratios)
+    axs[1].set_xlabel(r"$n_{opt} / n_{prop}$", fontsize=14)
+    axs[1].set_ylabel("Probability of Occurence", fontsize=14)
+    axs[1].xaxis.set_minor_locator(AutoMinorLocator())
+    axs[1].yaxis.set_minor_locator(AutoMinorLocator())
+    axs[1].set_xlim(0.85,1.0)
+    axs[1].tick_params(axis="both", which="major", labelsize=12)
+
+    plt.tight_layout()
+    plt.savefig("OptimalAllocTheoreticalSavings.png", dpi=300)
+
+def PlotSurface():
+    x = [0.05, 0.125, 0.25, 0.375, 0.5]
+    y = np.arange(100,1000, 1)
+
+    numPoints = {0.05:100, 0.125:500, 0.25:1000, 0.375:1500, 0.5:2000}
+    def func(x,y):
+        return numPoints[x] / y**2
+    
+    XX,YY = np.meshgrid(x,y)
+
+    zs = []
+    for i in range(len(np.ravel(XX))):
+        zs.append(func(np.ravel(XX)[i], np.ravel(YY)[i]))
+    zs = np.array(zs)
+    Z = zs.reshape(XX.shape)
+
+    mappable = plt.cm.ScalarMappable(cmap=plt.cm.viridis)
+    mappable.set_array(Z)
+    mappable.set_clim(0, max(zs+0.03)) # optional
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    surf = ax.plot_surface(XX*100, YY, Z, cmap=mappable.cmap, norm=mappable.norm, linewidth=0, antialiased=False)
+
+    surf = ax.plot_surface(XX*100, YY, Z+0.03, cmap=mappable.cmap, norm=mappable.norm, linewidth=0, antialiased=False)
+
+    ax.set_zlim(0,max(zs+0.05))
+    ax.set_xlabel('Porosity (%)')
+    ax.set_ylabel('Image Width (pixels)')
+    ax.set_zlabel('Sampling Fraction')
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+
+    plt.show()
+
+def PlotContour():
+    plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "Times",
+    "font.size":14
+    })
+
+    x = [0.05, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 0.95]
+    y = np.logspace(4,6.1, 20)
+
+    numPoints = {0.05:100, 0.125:500, 0.25:1000, 0.375:1500, 0.5:1750, 0.625:1500, 0.75: 1000, 0.875:500, 0.95: 100}
+    def func(x,y):
+        return numPoints[x] / y
+    
+    XX,YY = np.meshgrid(x,y)
+
+    zs = []
+    for i in range(len(np.ravel(XX))):
+        zs.append(func(np.ravel(XX)[i], np.ravel(YY)[i]))
+    zs = np.array(zs)
+    Z = zs.reshape(XX.shape)
+
+    # plot contour
+    plt.contourf(XX,YY,Z)
+    plt.xlabel("Porosity")
+    plt.ylabel("Number of Pixels")
+    CS = plt.contour(XX, YY, Z, [0.01, 0.05, 0.10])
+    plt.clabel(CS, colors="w")
+    plt.yscale("log")
+
+    # plot y-values where square micrograph sizes are
+    x = [0.05,0.95]
+    plt.plot(x, [256*256, 256*256], c="r", linestyle="--", linewidth=0.5)
+    plt.plot(x, [512*512, 512*512], c="r", linestyle="--", linewidth=0.5)
+    plt.plot(x, [1028*1028, 1028*1028], c="r", linestyle="--", linewidth=0.5)
+    plt.text(0.1, 250*250, "256 x 256", bbox={"edgecolor":"r", "facecolor":"w"})
+    plt.text(0.1, 500*500, "512 x 512", bbox={"edgecolor":"r", "facecolor":"w"})
+    plt.text(0.1, 1000*1000, "1024 x 1024", bbox={"edgecolor":"r", "facecolor":"w"})
+
+    plt.show()
+
 def TwoSidedCL_A(n, p, alpha):
     sum = 0
     A_U = 0
@@ -70,7 +254,7 @@ def TwoSidedCL_A(n, p, alpha):
         A_U += 1
         sum = 0
         for i in range(A_U):
-            sum += scipy.special.comb(n, i, exact=True) * np.power(p,i) * np.power((1-p),n-i)
+            sum += scipy.stats.binom.pmf(i, n, p)
 
     sum = 1.0
     A_L = 0
@@ -78,7 +262,7 @@ def TwoSidedCL_A(n, p, alpha):
         A_L += 1
         sum = 0
         for i in range(A_L, n+1):
-            sum += scipy.special.comb(n, i, exact=True) * np.power(p,i) * np.power((1-p),n-i)
+            sum += scipy.stats.binom.pmf(i, n, p)
     
     return A_L-1, A_U-1
 
@@ -92,7 +276,7 @@ def TopOneSidedCL_A(n, p, alpha):
         A_L += 1
         sum = 0
         for i in range(A_L, n+1):
-            sum += scipy.special.comb(n, i, exact=True) * np.power(p,i) * np.power((1-p),n-i)
+            sum += scipy.stats.binom.pmf(i, n, p)
     
     return A_L-1, A_U
 
@@ -103,7 +287,7 @@ def BottomOneSidedCL_A(n, p, alpha):
         A_U += 1
         sum = 0
         for i in range(A_U):
-            sum += scipy.special.comb(n, i, exact=True) * np.power(p,i) * np.power((1-p),n-i)
+            sum += scipy.stats.binom.pmf(i, n, p)
 
     A_L = 0
     
@@ -116,7 +300,7 @@ def UpperCL_A(n, p, alpha):
         A_U += 1
         sum = 0
         for i in range(A_U):
-            sum += scipy.special.comb(n, i, exact=True) * np.power(p,i) * np.power((1-p),n-i)
+            sum += scipy.stats.binom.pmf(i, n, p)
     
     return A_U-1
 
@@ -127,8 +311,12 @@ def LowerCL_A(n, p, alpha):
         A_L += 1
         sum = 0
         for i in range(A_L, n):
-            sum += scipy.special.comb(n, i, exact=True) * np.power(p,i) * np.power((1-p),n-i)
+            sum += scipy.stats.binom.pmf(i, n, p)
     
     return A_L-1
 
-PlotSamplesPerInitialGuess()
+# Plot_n_ratio()
+# PlotSurface()
+PlotContour()
+# PlotSampleFractionPerInitialConditions()
+# PlotSamplesPerInitialGuess()
